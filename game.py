68b256a3f0d0bffeee7598cfacfd47b94b13eaf4
apisit102 <son.bnk48@gmail.com>
2024-11-01 19:27:44 +0700

    screen

--- game.py
+++ game.py
@@ -1,2 +1,11 @@
-import game 
-screen = 600*400
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("DTI-ADVENTURE")
+
+while  True:
+	for event in pygame.event.get():
+		if event.type==pygame.QUIT:
+			quit()
+			FPS = 60 
+
